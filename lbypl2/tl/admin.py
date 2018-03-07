61e8bb2f922ddeceda8004ade04730a182a68f99
goldader <goldader@gmail.com>
2018-03-07 09:07:15 +0000

    Added json generator function and flattener functions

--- lbypl2/tl/admin.py
+++ lbypl2/tl/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
-from .models import Providers, TL_info, Token
+from .models import Providers, TL_info, Token, User_info, Tl_model_updates
 
 # Register your models here.
 
 admin.site.register(Providers)
 admin.site.register(TL_info)
 admin.site.register(Token)
+admin.site.register(User_info)
+admin.site.register(Tl_model_updates)
--- lbypl2/tl/models.py
+++ lbypl2/tl/models.py
@@ -9,12 +9,55 @@ from django.http import HttpRequest
 
 # Create your models here.
 
+def json_dict_generator(indict, pre=None):
+    # recurses a json file to flatten it out
+    pre = pre[:] if pre else []
+    if isinstance(indict, dict):
+        for key, value in indict.items():
+            if isinstance(value, dict):
+                for d in json_dict_generator(value, [key] + pre):
+                    yield d
+            elif isinstance(value, list) or isinstance(value, tuple):
+                for v in value:
+                    for d in json_dict_generator(v, [key] + pre):
+                        yield d
+            else:
+                yield pre + [key, value]
+    else:
+        yield pre + [indict]
+
+def json_output(json_input):
+    # creates an output file from nested json and creates related, named fields for db work
+    # todo check if this line is needed and if not remove - a = json_dict_generator(json_input)
+    dataset = {}
+    try:
+        a = json_dict_generator(json_input)
+        previous=None
+        count=0
+        while True:
+            value = next(a)
+            if len(value)==3:
+                value[0]+="_%s" % value[1]
+                value.pop(1)
+            if value[0].lower()==previous:
+                count+=1
+                value[0]+="_%s" % count
+            else:
+                count=0
+                previous=value[0].lower()
+            dataset[value[0].lower()] = value[1]
+    except StopIteration:
+        pass
+    finally:
+        return(dataset)
+
 class TL_info(models.Model):
     client_id = models.TextField(primary_key=True)
     app_name = models.CharField(max_length=250)
     client_secret = models.TextField()
     redirect_uri = models.TextField()
-    token_url = models.TextField(default='text')
+    token_url = models.TextField()
+    info_url = models.TextField()
 
     def __str__(self):
         return self.app_name
@@ -105,6 +148,10 @@ class Token(models.Model):
 
     def __str__(self):
         return ("UID : %s - Provider : %s" % (self.user,self.provider_id))
+
+    class Meta:
+        verbose_name = "User Token"
+        verbose_name_plural = "User Tokens"
 
     @classmethod
     def token_exists(cls, username, provider_id):
@@ -226,3 +273,43 @@ class Token(models.Model):
                 status = {'code': 400, 'desc': 'API failure. Test for TrueLayer connectivity to token request APIs.'}
                 return (status)
 
+class User_info(models.Model):
+    # gathers user information from TrueLayer as provided by the bank
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    provider_id = models.ForeignKey(Providers, unique=False, on_delete=models.DO_NOTHING)
+    update_timestamp = models.DateTimeField()
+    full_name = models.TextField()
+    address = models.TextField()
+    city = models.TextField()
+    zip = models.CharField(max_length=50)
+    country = models.CharField(max_length=150)
+    emails = models.TextField()
+    emails_1 = models.TextField()
+    emails_2 = models.TextField()
+    phones = models.CharField(max_length=50)
+    phones_2 = models.CharField(max_length=50)
+    phones_3 = models.CharField(max_length=50)
+    account_id = models.TextField()
+    account_type = models.TextField()
+    display_name = models.TextField()
+    currency = models.CharField(max_length=50)
+    account_number_iban = models.CharField(max_length=50)
+    account_number_swift_bic = models.CharField(max_length=15)
+    account_number_number = models.CharField(max_length=50)
+    account_number_sort_code = models.CharField(max_length=15)
+    provider_display_name = models.TextField()
+    provider_provider_id = models.CharField(max_length=250)
+    provider_logo_uri = models.TextField()
+
+    class Meta:
+        verbose_name = "User Information - Bank Provided"
+        verbose_name_plural = "User Information - Bank Provided"
+
+
+
+
+class Tl_model_updates(models.Model):
+    model_to_update = models.TextField()
+    fields_to_add = models.TextField()
+    method_to_recall = models.TextField()
+    details_to_pass = models.TextField()
